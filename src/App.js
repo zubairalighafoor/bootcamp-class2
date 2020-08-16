f202fa65b546efcd89e23e9516f83ea8915824f4
zubair <55566847+zubairhunjrah6004@users.noreply.github.com>
2020-08-16 09:57:26 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <header className="App-header">
+         <Dinner dishName="Baryani" sweetDish="Kheer"/>
+      </header>
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Dinner(props) {
+    return (
+    <h1>We are serving {props.dishName} and sweet {props.sweetDish}</h1>
+    )
+}
+export default Dinner
